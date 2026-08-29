184d462512fe0874a563e0d9c7664af5e07a9050
Jhon Ladines <Jhonjaimeladines17@gmail.com>
2026-08-29 21:08:52 +0800

    update the config file

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5013/api';
+const API_BASE_URL = 'https://piodelpilarmakatibackend.onrender.com/api';
 
 export const API_CONFIG = {
     BASE_URL: API_BASE_URL,
