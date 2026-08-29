d738100ef41a99d97efd4d1b029abde7f1f4247a
Jhon Ladines <Jhonjaimeladines17@gmail.com>
2026-08-29 21:30:39 +0800

    fix the docker

--- backend/Program.cs
+++ backend/Program.cs
@@ -9,18 +9,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.FileProviders;
 using System.Text;
 
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions
-{
-    Args = args,
-    ContentRootPath = Directory.GetCurrentDirectory()
-});
-
-// In production, only use environment variables to avoid file watching issues
-if (builder.Environment.IsProduction())
-{
-    builder.Configuration.Sources.Clear();
-    builder.Configuration.AddEnvironmentVariables();
-}
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
